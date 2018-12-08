29e3a621a167c0458caf46c1c16b52a2d8c36996
aras armani <aras.armani2014@gmail.com>
2018-12-08 09:52:02 +0300

    code cleaned

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Logo from './assets/img/vilaapp-logo.png'
 
@@ -27,34 +26,33 @@ import linkedin from './assets/img/linkedin.svg';
           </div> 
 
           <div className="email-container">
-            <h1><b>we are working on something</b> very interesting!</h1>
-            <h3><b>be notified.</b> we just need your email address.</h3>
-            <div className="email-input-container">
-              <div className="bg-fff">
-                  <input
-                  className="input-email"
-                  placeholder="Email"
-                  name="email"
-                  autocomplete="off"
-                  
-                  />
+              <h1><b>we are working on something</b> very interesting!</h1>
+              <h3><b>be notified.</b> we just need your email address.</h3>
 
-                <div className="btn-container">
-                  <span>Send</span>
-                </div>
+              <div className="email-input-container">
+                  <div className="bg-fff">
+                      <input
+                      className="input-email"
+                      placeholder="Email"
+                      name="email"
+                      autocomplete="off"
+                      
+                      />
+                      <div className="btn-container">
+                        <span>Send</span>
+                      </div>
+                  </div>
               </div>
-            </div>
-
           </div>
 
           <div className="socila-container">
-            <ul>
-              <li> <a href="https://www.linkedin.com/company/vilaapp/about/"  target="_blank" ><img src={linkedin} alt="vilaapp linkedin" height="25" width="25" /></a></li>
-              <li> <a href="https://www.facebook.com/vilaapp.ir/"  target="_blank" ><img src={facebook} alt="vilaapp facebook" height="25" width="25" /></a></li>
-              <li> <a href="https://www.instagram.com/VilaApp.ir/"  target="_blank" ><img src={instagram} alt="vilaapp instagram" height="25" width="25" /></a></li>
-              <li> <a href="https://twitter.com/vilaapp"  target="_blank" ><img src={twitter} alt="vilaapp twitter" height="25" width="25" /></a></li>
-    
-            </ul>
+              <ul>
+                <li> <a href="https://www.linkedin.com/company/vilaapp/about/"  target="_blank" ><img src={linkedin} alt="vilaapp linkedin" height="25" width="25" /></a></li>
+                <li> <a href="https://www.facebook.com/vilaapp.ir/"  target="_blank" ><img src={facebook} alt="vilaapp facebook" height="25" width="25" /></a></li>
+                <li> <a href="https://www.instagram.com/VilaApp.ir/"  target="_blank" ><img src={instagram} alt="vilaapp instagram" height="25" width="25" /></a></li>
+                <li> <a href="https://twitter.com/vilaapp"  target="_blank" ><img src={twitter} alt="vilaapp twitter" height="25" width="25" /></a></li>
+      
+              </ul>
           </div>
        </div>
       );
