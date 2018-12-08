0aea5a7d9f0626d7404db9f77395b34d14a5b64f
aras armani <aras.armani2014@gmail.com>
2018-12-08 08:35:39 +0300

    h1 and h3 is fiex

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Logo from './assets/img/vilaapp-logo.png'
 import facebook from './assets/img/facebook.svg';
 import instagram from './assets/img/instagram.svg';
 import twitter from './assets/img/twitter.svg';
+import linkedin from './assets/img/linkedin.svg';
 
 
  class Index extends Component {
@@ -26,8 +27,8 @@ import twitter from './assets/img/twitter.svg';
           </div> 
 
           <div className="email-container">
-            <h1>we are working on something very interesting!</h1>
-            <h3>be notified. we just need your email address.</h3>
+            <h1><b>we are working on something</b> very interesting!</h1>
+            <h3><b>be notified.</b> we just need your email address.</h3>
             <div className="email-input-container">
               <div className="bg-fff">
                   <input
@@ -47,6 +48,7 @@ import twitter from './assets/img/twitter.svg';
 
           <div className="socila-container">
             <ul>
+              <li> <a href="#" ><img src={linkedin} alt="vilaapp linkedin" height="25" width="25" /></a></li>
               <li> <a href="#" ><img src={facebook} alt="vilaapp facebook" height="25" width="25" /></a></li>
               <li> <a href="#" ><img src={instagram} alt="vilaapp instagram" height="25" width="25" /></a></li>
               <li> <a href="#" ><img src={twitter} alt="vilaapp twitter" height="25" width="25" /></a></li>
